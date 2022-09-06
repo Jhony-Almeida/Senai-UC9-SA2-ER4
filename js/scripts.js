dbcd91c7658d53918eadefe92fcb3fc899ed702e
Jhony Almeida <jhony12358oficial@gmail.com>
2022-09-06 13:13:43 -0300

    Encontro Remoto 4: JQuery

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+
+    $("[id^=comprar-click]").click(function() {
+        alert("Você acaba de Comprar este Produto!!!")
+        console.log("Você acaba de Comprar este Produto!!!")  
+    })
+
+})
---
+++ js/scripts_login.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+
+    $("#log").click(function() {
+        alert("Você logou com Suceesso!!!!!")
+        console.log("Você logou com Suceesso!!!!!")
+    })
+
+    $("#esquecer-senha").click(function() {
+        alert("Infelizmente não adicionamos nenhuma função para recuperar sua senha :(")
+        console.log("Infelizmente não adicionamos nenhuma função para recuperar sua senha :(")
+    })
+})
